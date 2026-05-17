e119897b881dc84088ef7b5d6b425551148d3e3a
skynet <skynetdigitalsolutionsug@gmail.com>
2026-05-17 16:12:33 +0300

    Version 2

---
+++ src/index.js
@@ -0,0 +1,14 @@
+function fan(w1,w2){
+    return w1 + w2;
+}
+console.log(fan(3434,543));
+
+const profile = {
+    name: 'Mpagi William',
+    age:20,
+    course:'Software Engineering',
+    hobbies: ['Coding', 'Gaming', 'Travelling'],
+    industry:'Digital Solutions'
+}
+
+console.log(profile);
